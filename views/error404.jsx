6e7abb5015ed78304bbff3d45c7c68428ce9f48d
cristian-nambo <cristian-nambo@hotmail.com>
2023-06-03 12:24:07 -0700

    Completed Step 5, need step6

--- views/error404.jsx
+++ views/error404.jsx
@@ -4,13 +4,21 @@ const Def = require('./default')
 function error404 () {
     return (
       <Def>
-          <main>
-              <h1>404: PAGE NOT FOUND</h1>
-              <p>Oops, sorry, we can't find this page!</p>
-          </main>
+<main>
+    <h1>404: PAGE NOT FOUND</h1>
+    <p>Oops, sorry, we can't find this page!</p>
+    <div>
+      <img src="/images/pitbull404.jpg" alt="NewCastle UnitedStates HDPitbullWallpaper" />
+    </div>
+    <div>
+        Photo by <a href="AUTHOR_LINK">Jane Almon</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+      </div>
+</main>
       </Def>
     )
   }
   
 
 module.exports = error404
+
+
