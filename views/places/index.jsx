47d3b395bc1805095b71521a3fed7ca0e024a650
cristian-nambo <cristian-nambo@hotmail.com>
2023-06-03 09:48:57 -0700

    Fixed bug in places

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,25 +1,25 @@
 const React = require('react')
-const Def = require('./default')
+const Def = require('../default')
 
 function index (data) {
+    console.log(data, "Data info")
   let placesFormatted = data.places.map((place) => {
     return (
-      <div>
-        <h2>{place.name}</h2>
-        <img src={place.pic} alt={place.name}/>
-      </div>
+        <div>
+            <h2>{place.name}</h2>
+            <img src={place.pic} alt={place.name}/>
+        </div>
     )
   })
-  return (
-    <Def>
-        <main>
-            <h1>PLACES INDEX PAGE</h1>
-            {placesFormatted}
-        </main>
-    </Def>
-)
-}
-
+    return (
+      <Def>
+          <main>
+              <h1>PLACES INDEX PAGE</h1>
+              {placesFormatted}
+          </main>
+      </Def>
+    )
+  }
   
 
 module.exports = index
