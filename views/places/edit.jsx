b5f2edf5a1ab62b4c12713f961887b905bcb6b3d
cristian-nambo <cristian-nambo@hotmail.com>
2023-06-08 21:23:22 -0700

    Fixed Data Edit and removed the required tag

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,16 +1,16 @@
 const React = require('react')
-const Def = require('../default.jsx')
+const Def = require('../default')
 
 function edit_form (data) {
     return (
         <Def>
           <main>
             <h1>Edit Place</h1>
-            <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+            <form method="POST" action={`/places/${data.id}?_method=PUT`}>
                     <div className="row">
                         <div className="form-group col-sm-6">
                         <label htmlFor="name">Place Name</label>
-                        <input className="form-control" id="name" name="name" value={data.place.name} required/>
+                        <input className="form-control" id="name" name="name" value={data.place.name}/>
                     </div>
                     <div className="form-group col-sm-6"> 
                         <label htmlFor="pic">Place Picture</label>
