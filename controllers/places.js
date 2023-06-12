a719d538e6588a9bc4736ae55250792c05e6548f
cristian-nambo <cristian-nambo@hotmail.com>
2023-06-11 20:56:41 -0700

    Implemented Mongoose ODM

--- controllers/places.js
+++ controllers/places.js
@@ -1,96 +1,174 @@
 const router = require('express').Router()
-const places = require('../models/places.js')
+const db = require('../models')
 
 //INDEX
 router.get('/', (req, res) => {
-    res.render('places/index', {places})
-})
-//NEW
-router.get("/new", (req, res) => {
-  res.render("places/new");
+    db.Place.find()
+    .then((places) => {
+      res.render('places/index', { places })
+    })
+    .catch(err => {
+      console.log(err) 
+      res.render('error404')
+    })
 })
 
-//POST
+//POST ROUTE
 router.post('/', (req, res) => {
-  if (!req.body.pic) {
-    req.body.pic = 'http://placekitten.com/400/400'
-  }
-  if (!req.body.city){
-    req.body.city = "Anytown"
-  }
-  if (!req.body.state) {
-    req.body.state = "USA"
-  }
-  places.push(req.body)
-  res.redirect('/places')
-})
-
-//CLICK LINK EDIT
-router.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
+  db.Place.create(req.body)
+  .then(() => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
       res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-    res.render('places/edit', {place: places[id], id})
-  }
+  })
 })
 
-//SHOW - ID - EDIT BUTTON
-router.put('/:id', (req, res)=>{
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    if (!req.body.pic) {
-      req.body.pic = 'http://placekitten.com/400/400'
-    }
-    if (!req.body.city){
-      req.body.city = 'Anytown'
-    }
-    if (!req.body.state) {
-      req.body.state = 'USA'
-    }
-    places[id] = req.body
-  res.redirect(`/places/${id}`)
-  }
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
 })
 
-//SHOW
+//SHOW ROUTE
 router.get('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    res.render('places/show', { place: places[id], id })
-  }
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/show', { place })
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
 
 
-//DELETE
+router.put('/:id', (req, res) => {
+  res.send('PUT /places/:id stub')
+})
+
 router.delete('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    places.splice(id, 1)
-  res.redirect('/places')
-  }
+  res.send('DELETE /places/:id stub')
+})
+
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
+
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
 })
 
 module.exports = router
+
+
+
+
+
+
+
+
+
+
+//BE_7_REST_RANT_PART8 ASKS TO DELETE ALL THE CONTENT OF THE ROUTES
+
+// const router = require('express').Router()
+// const places = require('../models/places.js')
+
+// //INDEX
+// router.get('/', (req, res) => {
+//     res.render('places/index', {places})
+// })
+// //NEW
+// router.get("/new", (req, res) => {
+//   res.render("places/new");
+// })
+
+// //POST
+// router.post('/', (req, res) => {
+//   if (!req.body.pic) {
+//     req.body.pic = 'http://placekitten.com/400/400'
+//   }
+//   if (!req.body.city){
+//     req.body.city = "Anytown"
+//   }
+//   if (!req.body.state) {
+//     req.body.state = "USA"
+//   }
+//   places.push(req.body)
+//   res.redirect('/places')
+// })
+
+// //CLICK LINK EDIT
+// router.get('/:id/edit', (req, res) => {
+//   let id = Number(req.params.id)
+//   if (isNaN(id)) {
+//       res.render('error404')
+//   }
+//   else if (!places[id]) {
+//       res.render('error404')
+//   }
+//   else {
+//     res.render('places/edit', {place: places[id], id})
+//   }
+// })
+
+// //SHOW - ID - EDIT BUTTON
+// router.put('/:id', (req, res)=>{
+//   let id = Number(req.params.id)
+//   if (isNaN(id)) {
+//     res.render('error404')
+//   }
+//   else if (!places[id]) {
+//     res.render('error404')
+//   }
+//   else {
+//     if (!req.body.pic) {
+//       req.body.pic = 'http://placekitten.com/400/400'
+//     }
+//     if (!req.body.city){
+//       req.body.city = 'Anytown'
+//     }
+//     if (!req.body.state) {
+//       req.body.state = 'USA'
+//     }
+//     places[id] = req.body
+//   res.redirect(`/places/${id}`)
+//   }
+// })
+
+// //SHOW
+// router.get('/:id', (req, res) => {
+//   let id = Number(req.params.id)
+//   if (isNaN(id)) {
+//     res.render('error404')
+//   }
+//   else if (!places[id]) {
+//     res.render('error404')
+//   }
+//   else {
+//     res.render('places/show', { place: places[id], id })
+//   }
+// })
+
+
+// //DELETE
+// router.delete('/:id', (req, res) => {
+//   let id = Number(req.params.id)
+//   if (isNaN(id)) {
+//     res.render('error404')
+//   }
+//   else if (!places[id]) {
+//     res.render('error404')
+//   }
+//   else {
+//     places.splice(id, 1)
+//   res.redirect('/places')
+//   }
+// })
+
+// module.exports = router
--- models/places.js
+++ models/places.js
@@ -1,13 +1,42 @@
-module.exports = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/thai.jpg'
-}, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/cafe.jpg'
-}]
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//BE7_REST_RANT_PART8 STEP 2 REQUIRES TO DELETE THIS PART.
+// module.exports = [{
+//     name: 'H-Thai-ML',
+//     city: 'Seattle',
+//     state: 'WA',
+//     cuisines: 'Thai, Pan-Asian',
+//     pic: '/images/thai.jpg'
+// }, {
+//     name: 'Coding Cat Cafe',
+//     city: 'Phoenix',
+//     state: 'AZ',
+//     cuisines: 'Coffee, Bakery',
+//     pic: '/images/cafe.jpg'
+// }]
