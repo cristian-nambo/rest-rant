4e216aaf4e4c89d818accd3fdbef01c2a2cb2d8e
cristian-nambo <cristian-nambo@hotmail.com>
2023-06-24 14:29:11 -0700

    Index rearranged

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-// Modules and Globals
-require('dotenv').config()
 const express = require('express')
 const app = express()
-const methodOverride = require ('method-override')
+const methodOverride = require('method-override')
 const mongoose = require('mongoose')
+//configuration
+require('dotenv').config()
 
-
-// Express Settings
+//middleware
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
-app.use(methodOverride('_method'))
-
-
-// Controllers & Routes
 app.use('/places', require('./controllers/places'))
+
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -31,5 +28,43 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
-// Listen for Connections
 app.listen(process.env.PORT)
+
+
+
+
+// // Modules and Globals
+// require('dotenv').config()
+// const express = require('express')
+// const app = express()
+// const methodOverride = require ('method-override')
+// const mongoose = require('mongoose')
+
+
+// // Express Settings
+// app.set('views', __dirname + '/views')
+// app.set('view engine', 'jsx')
+// app.engine('jsx', require('express-react-views').createEngine())
+// app.use(express.static('public'))
+// app.use(express.urlencoded({extended:true}))
+// app.use(methodOverride('_method'))
+
+
+// // Controllers & Routes
+// app.use('/places', require('./controllers/places'))
+
+// app.get('/', (req, res) => {
+//     res.render('home')
+// })
+
+// const PORT = process.env.PORT || 3000
+// mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
+//     () => {console.log('connected to : ', process.env.MONGO_URI)}
+//     )
+
+// app.get('*', (req, res) => {
+//     res.render('error404')
+// })
+
+// // Listen for Connections
+// app.listen(process.env.PORT)
