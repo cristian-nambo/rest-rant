46846080eef3a1b1106f646c78fee3a1b1e1e5a1
cristian-nambo <cristian-nambo@hotmail.com>
2023-06-24 16:10:36 -0700

    Index 1.1

--- index.js
+++ index.js
@@ -1,38 +1,3 @@
-// const express = require('express')
-// const app = express()
-// const methodOverride = require('method-override')
-// const mongoose = require('mongoose')
-// //configuration
-// require('dotenv').config()
-
-// //middleware
-// app.set('views', __dirname + '/views')
-// app.set('view engine', 'jsx')
-// app.engine('jsx', require('express-react-views').createEngine())
-// app.use(methodOverride('_method'))
-// app.use(express.static('public'))
-// app.use(express.urlencoded({extended:true}))
-// app.use('/places', require('./controllers/places'))
-
-
-// app.get('/', (req, res) => {
-//     res.render('home')
-// })
-
-// const PORT = process.env.PORT || 3000
-// mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
-//     () => {console.log('connected to : ', process.env.MONGO_URI)}
-//     )
-
-// // 404 page
-// app.get('*', (req, res) => {
-//     res.render('error404')
-// })
-
-// app.listen(process.env.PORT)
-
-
-
 
 // Modules and Globals
 require('dotenv').config()
@@ -58,10 +23,9 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-const PORT = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
-    () => {console.log('connected to : ', process.env.MONGO_URI)}
-    )
+  () => {console.log('connected to : ', process.env.MONGO_URI)})
 
 app.get('*', (req, res) => {
     res.render('error404')
